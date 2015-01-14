db2f07b8d33d6faf1a105c3938036ed4aca934c9
Paul Jones <shmiko@gmail.com>
2015-01-14 22:31:26 +1100

    CalendarAngularApp Baseline

---
+++ server/config.js
@@ -0,0 +1,11 @@
+'use strict';
+//================================================================================
+// Module
+//================================================================================
+module.exports = {
+    google: {
+        clientId: '407717796667-7euei2g372vo63u7blvkl1pcb5vvsald.apps.googleusercontent.com',
+        clientSecret: 'zGOSTDXuOi4lLUeJuEUyxV7D',
+        callbackURL: 'http://localhost:7000/auth/google/callback'
+    }
+};
